c768d1d16d98d5f361c5db4a28b3fd6e2ec904bc
Benjamin Yin <fellfromatree@gmail.com>
2016-08-27 20:04:12 -0700

    Should've committed progress a long time before this. Whoops. First "working" version.

--- Supervisor.py
+++ Supervisor.py
@@ -1,9 +1,11 @@
+import json
 import pickle
 import random
 import argparse
+import copy
+from individual import Individual, Node
 
-
-def main(popCount = 100, genMax = 1000, checkpoint=None):
+def main(popCount = 100, genMax = None, checkpoint=None):
     gen = 0
     population = []
     if checkpoint:
@@ -13,7 +15,8 @@ def main(popCount = 100, genMax = 1000, checkpoint=None):
                 data = pickle.load(cp_file)
             if data["population"]:
                 gen=data["gen"]
-                genMax=data["genMax"]
+                if not genMax:
+                    genMax=data["genMax"]
                 population = data["population"]
             else:
                 population = generate(popCount)
@@ -24,6 +27,8 @@ def main(popCount = 100, genMax = 1000, checkpoint=None):
         population = generate(popCount)
     try:
         while gen < genMax:
+            if gen % 100 == 0:
+                save(checkpoint, population, gen, genMax)
             random.shuffle(population)
             for p1, p2 in pairwise(population):
                 play(p1,p2)
@@ -32,52 +37,157 @@ def main(popCount = 100, genMax = 1000, checkpoint=None):
             gen += 1
     except (KeyboardInterrupt, SystemExit):
         if checkpoint:
-            with open(checkpoint, "w+b") as f:
-                data = {};
-                data["gen"] = gen
-                data["genMax"] = genMax
-                data["population"] = population
-                pickle.dump(data, f)
-                print("Population saved! Thanks!")
+            save(checkpoint, population, gen, genMax)
         else:
             checkpoint = input("If you want to save this, input a file name now.")
             if len(checkpoint) > 0:
-                with open(checkpoint, "w+b") as f:
-                    data = {};
-                    data["gen"] = gen
-                    data["genMax"] = genMax
-                    data["population"] = population
-                    pickle.dump(data, f)
-                    print("Population saved! Thanks!")
-
+                save(checkpoint, population, gen, genMax)
+    stats = []
+    population.sort(key=evalFitness)
+    for x in population[:int(len(population)/4)]:
+        stats.append(x.stats())
+    if checkpoint:
+        with open(checkpoint + ".res", "w") as f:
+            json.dump(stats, f, indent=4, sort_keys=True)
 def generate(popCount):
     print("generated popcount things");
     l = []
+    starting = [0,0,0,0,1,1,1,1]
     for x in range(popCount):
-        l.append(x)
+        moveDir = (0,0)
+        while moveDir ==(0,0):
+            moveDir = (random.randint(-1,1),random.randint(-1,1))
+        root = Node(random.randrange(6),random.randrange(8),bool(random.getrandbits(1)),moveDirection=moveDir)
+        l.append(Individual(root,random.sample(starting,8)))
     return l
 
 def play(p1,p2):
-    print(str(p1) + " and " + str(p2))
-    total = p1+p2
-    rand = random.random()
-    if(rand < p1/total):
-        p1+=1
-    else:
-        p2+=1
-
+    board = {}
+    for x in range(8):
+        for y in range(8):
+            board[x,y] = 0
+    p1CapturedGood = 0
+    p1CapturedBad = 0
+    p2CapturedGood = 0
+    p2CapturedBad = 0
+    for x in range(2,6):
+        board[x,0] = p1.starting[x-2]
+        board[x,1] = p1.starting[x+2]
+        board[x,7] = -p2.starting[x-2]
+        board[x,6] = -p2.starting[x+2]
+    while True:
+        for x in range(100):
+            move = p1.next(board, False)
+            if x==100 or move[0] == False:
+                p2.win+=1
+                p1.loss+=1
+                p1.capturedBad += p1CapturedBad
+                p1.capturedGood += p1CapturedGood
+                p2.capturedBad += p2CapturedBad
+                p2.capturedGood += p2CapturedGood
+                return
+            if board[move[1]] < 1:
+                break;
+        piece = board[move[0]]
+        board[move[0]] = 0
+        if board[move[1]] == -2:
+            p2CapturedBad+=1
+        elif board[move[1]] == -1:
+            p2CapturedGood+=1
+        board[move[1]] = piece
+        if board[0,7] == 1 or board[7,7] == 1 or p1CapturedBad == 4 :
+            p1.win+=1
+            p1.sneakyWin += 1
+            p2.loss+=1
+            p1.capturedBad += p1CapturedBad
+            p1.capturedGood += p1CapturedGood
+            p2.capturedBad += p2CapturedBad
+            p2.capturedGood += p2CapturedGood
+            return
+        elif p2CapturedGood == 4:
+            p1.win+=1
+            p2.loss+=1
+            p1.capturedBad += p1CapturedBad
+            p1.capturedGood += p1CapturedGood
+            p2.capturedBad += p2CapturedBad
+            p2.capturedGood += p2CapturedGood
+            return
+        for x in range(100):
+            move = p2.next(board, True)
+            if x == 100 or move[0] == False:
+                p1.win+=1
+                p2.loss+=1
+                p1.capturedBad += p1CapturedBad
+                p1.capturedGood += p1CapturedGood
+                p2.capturedBad += p2CapturedBad
+                p2.capturedGood += p2CapturedGood
+                return
+            if board[move[1]] > -1:
+                break;
+        piece = board[move[0]]
+        board[move[0]] = 0
+        if board[move[1]] == 2:
+            p2CapturedBad+=1
+        elif board[move[1]] == 1:
+            p2CapturedGood+=1
+        board[move[1]] = piece
+        if p2CapturedBad == 4 or board[0,0] == -1 or board[0,7] == -1:
+            p2.win+=1
+            p2.sneakyWin += 1
+            p1.loss+=1
+            p1.capturedBad += p1CapturedBad
+            p1.capturedGood += p1CapturedGood
+            p2.capturedBad += p2CapturedBad
+            p2.capturedGood += p2CapturedGood
+            return
+        elif p1CapturedGood == 4:
+            p2.win+=1
+            p1.loss+=1
+            p1.capturedBad += p1CapturedBad
+            p1.capturedGood += p1CapturedGood
+            p2.capturedBad += p2CapturedBad
+            p2.capturedGood += p2CapturedGood
+            return
 def evalFitness(individual):
-    return -individual
+    return 1 - individual.win/(individual.win + individual.loss)
 
-def recombine(population):
-    for x in range(len(population)):
-        if population[x]/10 < random.random():
-            population[x] = random.randint(1,10)
-    print("new population " + str(population))
+def recombine(population, mutChance = 0.25):
+    selectionIndex = 25  #int(len(population)/4)
+    for x in range(selectionIndex, len(population)):
+        parent = population[random.randrange(selectionIndex)].copy()
+        graftOn = random.choice(parent[1])
+        graftee = random.choice(population[random.randrange(selectionIndex)].copy()[1])
+        if bool(random.getrandbits(1)):
+            graftOn.passed = graftee
+        else:
+            graftOn.failed = graftee
+        if random.random() < mutChance:
+            choice = random.randrange(6)
+            unlucky = random.choice(parent[1])
+            if choice == 1:
+                unlucky.failed = False
+            elif choice == 2:
+                unlucky.passed = False
+            elif choice == 3:
+                unlucky.cutoff = random.randrange(8)
+            elif choice == 4:
+                unlucky.direction = bool(random.getrandbits(1))
+            elif choice == 5:
+                unlucky.selector = random.randrange(6)
+    print("new population, best" + str(population[0].win/(population[0].win+population[0].loss)))
 
 def pairwise(iterable):
     a = iter(iterable)
     return zip(a, a)
+
+def save(checkpoint, population, gen, genMax):
+    with open(checkpoint + ".sav", "w+b") as f:
+        data = {};
+        data["gen"] = gen
+        data["genMax"] = genMax
+        data["population"] = population
+        pickle.dump(data, f)
+        print("Population saved! Thanks!")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
---
+++ individual.py
@@ -0,0 +1,192 @@
+import operator
+from collections import deque
+def selectClosest(board):
+    distance = 0
+    selected = False
+    for item in board.items():
+        if item[1] > 0:
+            for other in board.items():
+                if other[1] < 0:
+                    store = abs(item[0][0] - other[0][0]) + abs(item[0][1] - other[0][1])
+                    if store > distance:
+                        distance = store
+                        selected = item[0]
+    return (selected, distance)
+def selectFarthest(board):
+    distance = 100
+    selected = False
+    for item in board.items():
+        if item[1] > 0:
+            for other in board.items():
+                if other[1] < 0:
+                    store = abs(item[0][0] - other[0][0]) + abs(item[0][1] - other[0][1])
+                    if store < distance:
+                        distance = store
+                        selected = item[0]
+    return (selected, distance)
+def selectClosestInverted(board):
+    distance = 0
+    selected = False
+    for item in board.items():
+        if item[1] < 0:
+            for other in board.items():
+                if other[1] > 0:
+                    store = abs(item[0][0] - other[0][0]) + abs(item[0][1] - other[0][1])
+                    if store > distance:
+                        distance = store
+                        selected = item[0]
+    return (selected, distance)
+def selectFarthestInverted(board):
+    distance = 100
+    selected = False
+    for item in board.items():
+        if item[1] < 0:
+            for other in board.items():
+                if other[1] > 0:
+                    store = abs(item[0][0] - other[0][0]) + abs(item[0][1] - other[0][1])
+                    if store < distance:
+                        distance = store
+                        selected = item[0]
+    return (selected, distance)
+def backmost(board):
+    selected = (7,7)
+    for key in board:
+        if key[1] < selected [1] and board[key] > 0:
+            selected = key
+    return (selected, selected[1])
+def backmostInverted(board):
+    selected = (0,0)
+    for key in board:
+        if key[1] > selected [1] and board[key] < 0:
+            selected = key
+    return (selected, 7-selected[1])
+def leftmost(board):
+    selected = (7,7)
+    for key in board:
+        if key[0] < selected [0] and board[key] > 0:
+            selected = key
+    return (selected, selected[0])
+def leftmostInverted(board):
+    selected = (0,0)
+    for key in board:
+        if key[0] > selected [0] and board[key] < 0:
+            selected = key
+    return (selected, 7-selected[0])
+def frontmost(board):
+    selected = (7,7)
+    for key in board:
+        if key[1] < selected [1] and board[key] > 0:
+            selected = key
+    return (selected, selected[1])
+def frontmostInverted(board):
+    selected = (0,0)
+    for key in board:
+        if key[1] > selected [1] and board[key] < 0:
+            selected = key
+    return (selected, 7-selected[1])
+def rightmost(board):
+    selected = (0,0)
+    for key in board:
+        if key[0] > selected [0] and board[key] > 0:
+            selected = key
+    return (selected, 7-selected[0])
+def rightmostInverted(board):
+    selected = (7,7)
+    for key in board:
+        if key[0] < selected [0] and board[key] < 0:
+            selected = key
+    return (selected, selected[0]) 
+selectors = [selectClosest, selectFarthest, backmost, frontmost, leftmost, rightmost, selectClosestInverted, selectFarthestInverted, backmostInverted, frontmostInverted, leftmostInverted, rightmostInverted]
+
+
+class Individual:
+    def __init__(this, root, starting):
+        this.root = root
+        this.current = root
+        this.starting = starting
+        this.capturedBad = 0
+        this.capturedGood = 0
+        this.sneakyWin = 0  
+        this.win = 0
+        this.loss = 0
+
+    def next(this, board,invert):
+        for x in range(100):
+            result = this.current.next(board, invert)
+            if result[1]:
+                this.current = result[1]
+            else:
+                this.current = this.root
+            if this.current.moveDirection:
+                move = tuple(map(operator.add, result[0], this.current.moveDirection))
+                if move[0] > 7 or move[0] < 0 or move[1] > 7 or move[1] < 0:
+                    continue
+                return (result[0], move)
+        return (False,False)
+    def copy(this):
+        nodes = this.root.copy()
+        result = Individual(nodes[-1], this.starting)
+        return (result, nodes)
+    def stats(this):
+        states = {}
+        try:
+            states["winRatio"] = this.win/(this.win+this.loss)
+            states["sneakiness"] = this.sneakyWin/(this.win+this.loss) + this.capturedBad/(4*(this.capturedBad + this.capturedGood))
+            states["starting"] = this.starting
+        except:
+            states["sneakiness"] = "You definitely did not run the alg for long enough. Shame. Shaaaame."
+        return states
+
+class Node:
+    def __init__(this, selector, cutoff, direction, moveDirection = None, passed = None, failed = None):
+        this.selector = selector
+        this.cutoff = cutoff
+        this.direction = direction
+        if moveDirection:
+            this.moveDirection = moveDirection
+        else:
+            this.moveDirection = False
+        if passed:
+            this.passed = passed
+        else:
+            this.passed = False
+        if failed:
+            this.failed = failed
+        else:
+            this.failed = False
+    def next(this, board, invert):
+        result = selectors[(this.selector + 6) if invert else this.selector](board)
+        if result == False:
+            if this.failed:
+                return((-10,-10), this.failed)
+            else:
+                return((-10,-10), False)
+        if (result[1] < this.cutoff) == this.direction:
+            if this.passed:
+                return (result[0], this.passed)
+            else:
+                return (result[0], False)
+        else:
+            if this.failed:
+                return(result[0], this.failed)
+            else:
+                return(result[0], False)
+    def copy(this):
+        result = Node.__new__(this.__class__)
+        result.__dict__.update(this.__dict__)
+        nodes = False
+        if this.passed:
+            nodes = this.passed.copy()
+            result[x].passed = nodes[-1]
+        if this.failed:
+            faileds = this.failed.copy()
+            if nodes:
+                nodes.extend(faileds)
+            else:
+                nodes = faileds
+            result[x].failed = nodes[-1]
+        if nodes:
+            nodes.append(result)
+        else:
+            nodes = [result]
+        return nodes
